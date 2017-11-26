7172a3cbf121ccb99eee66c804761c291b45a433
g ko <gim.kobayashi@gmail.com>
2017-11-26 11:21:03 +0000

    a

--- download-dataset.sh
+++ download-dataset.sh
@@ -1,1 +1,5 @@
-wget https://storage.googleapis.com/nardtree/jin115-20171125
+wget https://storage.googleapis.com/nardtree/jin115-20171125/contents.tar.bz2
+wget https://storage.googleapis.com/nardtree/jin115-20171125/contents.zip
+wget https://storage.googleapis.com/nardtree/jin115-20171125/jin115-20171125.zip
+wget https://storage.googleapis.com/nardtree/jin115-20171125/wakati-verbs.tar.gz
+wget https://storage.googleapis.com/nardtree/jin115-20171125/wakati.tar.gz
